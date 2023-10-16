779c250657e7cf99e04fa22c2db0afad3ce4050f
chienpham1605 <107631195+chienpham1605@users.noreply.github.com>
2023-10-16 19:26:13 +0700

    git update followed by areas

---
+++ Source/Client/Areas/Authentication/Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PostOffice.Client.Areas.Authentication.Controllers
+{
+    [Area("Authentication")]
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
