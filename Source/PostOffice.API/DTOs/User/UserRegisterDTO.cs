95cc1d010d36bc89f00cd2e0869409c906a31420
Nguyen Phuong Hoa <nguyenphuonghoa0709@gmail.com>
2023-10-26 23:53:38 +0700

    update valid phone and add pincode

--- Source/PostOffice.API/DTOs/User/UserRegisterDTO.cs
+++ Source/PostOffice.API/DTOs/User/UserRegisterDTO.cs
@@ -5,7 +5,7 @@ namespace PostOffice.API.DTOs.User
     public class UserRegisterDTO
     {
         [Required(ErrorMessage = "Please enter the first name")]
-        [StringLength(maximumLength: 25, MinimumLength = 3, ErrorMessage = "Length must be between 10 to 25")]
+        [StringLength(maximumLength: 25, MinimumLength = 1, ErrorMessage = "Length must be between 1 to 25")]
         public string FirstName { get; set; } = null!;
         [Required(ErrorMessage = "Please enter the last name")]
         public string LastName { get; set; } = null!;
@@ -14,7 +14,8 @@ namespace PostOffice.API.DTOs.User
 
         [Required, EmailAddress(ErrorMessage = "Please enter a valid email")]            
         public string Email { get; set; } = null!;
-        [Required(ErrorMessage = "Password is a required field.")]             
+        [Required(ErrorMessage = "Password is a required field.")]
+        [StringLength(maximumLength: 15, MinimumLength = 6, ErrorMessage = "Length must be between 6 to 15")]
         public string Password { get; set; } = null!;
         [Required(ErrorMessage = "Confirm password is a required field.")]
         [Compare("Password", ErrorMessage = "The new password and confirm password do not match.")]
@@ -23,7 +24,8 @@ namespace PostOffice.API.DTOs.User
         public string PincodeId { get; set; }
         [Required]
         public string Address { get; set; }
-        [Phone(ErrorMessage = "Please enter a valid Phone No")]
+        [Required]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Valid phone number must has 10 to 15 number")]
         public string PhoneNumber { get; set; } = null!;
         [Required]
         public DateTime Create_date { get; set; }
